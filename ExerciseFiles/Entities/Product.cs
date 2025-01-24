4dfa83e1eb185a295e5428bbebcabd43f0f442c2
Fabricio Dias <fabricio270909@gmail.com>
2025-01-23 23:14:45 -0300

    Entity structured

---
+++ ExerciseFiles/Entities/Product.cs
@@ -0,0 +1,24 @@
+namespace ExerciseFiles.Entities;
+
+public class Product
+{
+    public string Name { get; set; }
+    public double Price { get; set; }
+    public int Quantity { get; set; }
+
+    public Product()
+    {
+    }
+
+    public Product(string name, double price, int quantity)
+    {
+        Name = name;
+        Price = price;
+        Quantity = quantity;
+    }
+
+    public double TotalPrice(double price, int quantity)
+    {
+        return quantity * price;
+    }
+}
